7f096b96ecfff52e3e41c055f29106f99c53e0a9
Schumova Maria <schumova.maria@gmail.com>
2023-02-04 12:41:53 +0300

    11. Добавляет a11y для swiper

---
+++ source/js/modules/sliders/sliders-a11y.js
@@ -0,0 +1,9 @@
+// Используйте при инициализации слайдера: https://swiperjs.com/swiper-api#accessibility-a11y
+
+export const a11y = {
+  firstSlideMessage: 'Это первый слайд',
+  lastSlideMessage: 'Это последний слайд',
+  paginationBulletMessage: 'Переход к слайду {{index}}',
+  prevSlideMessage: 'Предыдущий слайд',
+  nextSlideMessage: 'Следующий слайд',
+};
