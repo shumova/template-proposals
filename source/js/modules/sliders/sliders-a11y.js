85897f31316c30b6b1a61c9c70c303ccfc620a86
Schumova Maria <schumova.maria@gmail.com>
2023-02-06 15:53:14 +0300

    14. Убирает a11y для swiper

--- source/js/modules/sliders/sliders-a11y.js
+++
@@ -1,9 +0,0 @@
-// Используйте при инициализации слайдера: https://swiperjs.com/swiper-api#accessibility-a11y
-
-export const a11y = {
-  firstSlideMessage: 'Это первый слайд',
-  lastSlideMessage: 'Это последний слайд',
-  paginationBulletMessage: 'Переход к слайду {{index}}',
-  prevSlideMessage: 'Предыдущий слайд',
-  nextSlideMessage: 'Следующий слайд',
-};
